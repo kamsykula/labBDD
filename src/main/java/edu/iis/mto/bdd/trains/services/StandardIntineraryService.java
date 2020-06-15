4657eb59e43811560ebd6f9f9d5221aa50dd5d27
Asmei <kamsykula@gmail.com>
2020-06-15 18:33:03 +0200

    feature1_impl Added dependency to StandardIntineraryService
    fadded some stuff to test

--- src/main/java/edu/iis/mto/bdd/trains/services/StandardIntineraryService.java
+++ src/main/java/edu/iis/mto/bdd/trains/services/StandardIntineraryService.java
@@ -1,13 +1,35 @@
 package edu.iis.mto.bdd.trains.services;
 
+import edu.iis.mto.bdd.trains.model.Line;
+import org.joda.time.Duration;
 import org.joda.time.LocalTime;
+import org.joda.time.Period;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class StandardIntineraryService implements IntineraryService{
+public class StandardIntineraryService implements IntineraryService {
+	private final Period timeWindow;
+	private final TimetableService timetableService;
+	
+	public StandardIntineraryService(TimetableService timetableService, Period timeWindow) {
+		this.timetableService = timetableService;
+		this.timeWindow = timeWindow;
+	}
 	
 	@Override
 	public List<LocalTime> findNextDepartures(String departure, String destination, LocalTime startTime) {
-		return null;
+		List<LocalTime> rV = new LinkedList<>();
+		
+		for(Line line : timetableService.findLinesThrough(departure, destination)){
+			rV.addAll(timetableService.findArrivalTimes(line, destination)
+					.stream()
+					.filter(time -> time.isAfter(startTime))
+					.filter(time -> time.plus(timeWindow).isAfter(startTime))
+					.collect(Collectors.toList()));
+		}
+		return rV;
 	}
+	
 }
--- src/test/java/edu/iis/mto/bdd/trains/junit/WhenCalculatingArrivalTimes.java
+++ src/test/java/edu/iis/mto/bdd/trains/junit/WhenCalculatingArrivalTimes.java
@@ -1,4 +1,37 @@
 package edu.iis.mto.bdd.trains.junit;
 
+import edu.iis.mto.bdd.trains.services.IntineraryService;
+import edu.iis.mto.bdd.trains.services.StandardIntineraryService;
+import edu.iis.mto.bdd.trains.services.TimetableService;
+import org.hamcrest.MatcherAssert;
+import org.joda.time.LocalTime;
+import org.jruby.embed.Extension;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.exceptions.base.MockitoException;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 public class WhenCalculatingArrivalTimes {
+	
+	@Mock
+	private TimetableService timetableService;
+	private IntineraryService service;
+	
+	
+	@Before
+	public void beforeEach() {
+		service = new StandardIntineraryService(timetableService);
+	}
+	
+	@Test
+	public void findTheNextProperStationShouldReturnListWithOneArrivalTime() {
+		List<LocalTime> times = service.findNextDepartures("Station 0", "Station 1", LocalTime.parse("12:00"));
+		assertThat(times, is(List.of(LocalTime.parse("12:05"))));
+	}
+	
 }
