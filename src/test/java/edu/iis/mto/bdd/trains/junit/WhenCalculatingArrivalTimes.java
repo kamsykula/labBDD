009638c14004cf6eeea4f38eb00eafa23c1c8ef0
Asmei <kamsykula@gmail.com>
2020-06-15 19:09:30 +0200

    feature1_impl first valid test

--- src/test/java/edu/iis/mto/bdd/trains/junit/WhenCalculatingArrivalTimes.java
+++ src/test/java/edu/iis/mto/bdd/trains/junit/WhenCalculatingArrivalTimes.java
@@ -1,37 +1,53 @@
 package edu.iis.mto.bdd.trains.junit;
 
+import edu.iis.mto.bdd.trains.model.Line;
 import edu.iis.mto.bdd.trains.services.IntineraryService;
 import edu.iis.mto.bdd.trains.services.StandardIntineraryService;
 import edu.iis.mto.bdd.trains.services.TimetableService;
 import org.hamcrest.MatcherAssert;
 import org.joda.time.LocalTime;
+import org.joda.time.Period;
 import org.jruby.embed.Extension;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.exceptions.base.MockitoException;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class WhenCalculatingArrivalTimes {
 	
-	@Mock
 	private TimetableService timetableService;
 	private IntineraryService service;
 	
 	
 	@Before
 	public void beforeEach() {
-		service = new StandardIntineraryService(timetableService);
+		timetableService = mock(TimetableService.class);
+		service = new StandardIntineraryService(timetableService, Period.minutes(15));
 	}
 	
 	@Test
 	public void findTheNextProperStationShouldReturnListWithOneArrivalTime() {
-		List<LocalTime> times = service.findNextDepartures("Station 0", "Station 1", LocalTime.parse("12:00"));
-		assertThat(times, is(List.of(LocalTime.parse("12:05"))));
+		LocalTime startTime = LocalTime.now();
+		LocalTime arrivalTime = startTime.plusMinutes(5);
+		
+		when(timetableService.findLinesThrough(any(String.class), any(String.class)))
+				.thenReturn(Collections.singletonList(new Line.LineBuilder("Line").departingFrom("Station 0").withStations("Station 1")));
+		when(timetableService.findArrivalTimes(any(Line.class), any(String.class)))
+				.thenReturn(Collections.singletonList(arrivalTime));
+		
+		List<LocalTime> times = service.findNextDepartures("Station 0", "Station 1", startTime);
+		assertThat(times, is(List.of(arrivalTime)));
 	}
 	
 }
